857be420604b83f8b14eb7edfafbbeebc5f35963
猫先生 <im.jp@outlook.com>
2021-12-07 21:53:15 +0800

    add unit test

--- src/Test/Hosted/UploadControllerTest.cs
+++ src/Test/Hosted/UploadControllerTest.cs
@@ -1,14 +1,53 @@
+using Autofac.Extras.Moq;
+using FileServer.Hosted;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Test.Hosted
 {
     public class UploadControllerTest
     {
+        /// <summary>
+        /// https://stackoverflow.com/questions/67517162/testing-a-multipart-file-upload-azure-function
+        /// https://darchuk.net/2019/03/29/asp-net-core-unit-testing-a-file-upload/
+        /// </summary>
+        /// <returns></returns>
         [Fact]
-        public void Test1()
+        public async Task StreamUploadTest()
         {
+            //var fileNames = "testfile2.jpg";
+            //var formFiles = HttpRequestFactory.GetFormFiles(fileNames);
+            //var formCollection = new FormCollection(null, formFiles);
 
+            // Get a loose automock
+            var Mock = AutoMock.GetLoose();
+            string expectedFileContents = "This is the expected file contents!";
+            // Set up the form file with a stream containing the expected file contents
+            Mock<IFormFile> formFiles = new Mock<IFormFile>();
+            formFiles.Setup(ff => ff.CopyToAsync(It.IsAny<Stream>(), It.IsAny<CancellationToken>()))
+              .Returns<Stream, CancellationToken>((s, ct) =>
+              {
+                  byte[] buffer = Encoding.Default.GetBytes(expectedFileContents);
+                  s.Write(buffer, 0, buffer.Length);
+                  return Task.CompletedTask;
+              });
+
+            // Set up the form collection with the mocked form
+            Mock<IFormCollection> formCollection = new Mock<IFormCollection>();
+            formCollection.Setup(f => f.Files[It.IsAny<int>()]).Returns(formFiles.Object);
+
+            var request = HttpRequestFactory.Create("POST", "multipart/form-data", formCollection.Object);
+            request.EnableBuffering();
+            var response = await UploadHelper.StreamUpload(request);
+
+            Assert.True(!string.IsNullOrEmpty(response));
         }
     }
 }
---
+++ src/Test/HttpRequestFactory.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.IO;
+
+namespace Test
+{
+    public static class HttpRequestFactory
+    {
+        public static HttpRequest Create(string method, string contentType, IFormCollection formCollection)
+        {
+            var context = new DefaultHttpContext();
+            var request = context.Request;
+            var boundary = $"----------------------------{DateTime.Now.Ticks.ToString("x")}";
+
+            request.Method = method;
+            request.Headers.Add("Cache-Control", "no-cache");
+            request.Headers.Add("Content-Type", contentType);
+            request.ContentType = $"{contentType}; boundary={boundary}";
+            request.Form = formCollection;
+
+
+            return request;
+        }
+
+        public static FormFileCollection GetFormFiles(string fileNames)
+        {
+            var formFileCollection = new FormFileCollection();
+            foreach (var file in fileNames.Split(','))
+            {
+                formFileCollection.Add(GetFormFile(file));
+            }
+            return formFileCollection;
+        }
+
+        private static IFormFile GetFormFile(string fileName)
+        {
+            string fileExtension = fileName.Substring(fileName.IndexOf('.') + 1);
+            string fileNameandPath = GetFilePathWithName(fileName);
+            IFormFile formFile;
+            var stream = File.OpenRead(fileNameandPath);
+
+            switch (fileExtension)
+            {
+                case "jpg":
+                    formFile = new FormFile(stream, 0, stream.Length,
+                        fileName.Substring(0, fileName.IndexOf('.')),
+                        fileName)
+                    {
+                        Headers = new HeaderDictionary(),
+                        ContentType = "image/jpeg"
+                    };
+                    break;
+
+                case "png":
+                    formFile = new FormFile(stream, 0, stream.Length,
+                        fileName.Substring(0, fileName.IndexOf('.')),
+                        fileName)
+                    {
+                        Headers = new HeaderDictionary(),
+                        ContentType = "image/png"
+                    };
+                    break;
+
+                case "pdf":
+                    formFile = new FormFile(stream, 0, stream.Length,
+                        fileName.Substring(0, fileName.IndexOf('.')),
+                        fileName)
+                    {
+                        Headers = new HeaderDictionary(),
+                        ContentType = "application/pdf"
+                    };
+                    break;
+
+                default:
+                    formFile = null;
+                    break;
+            }
+
+            return formFile;
+        }
+
+        private static string GetFilePathWithName(string filename)
+        {
+            var outputFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            return $"{outputFolder.Substring(0, outputFolder.IndexOf("bin"))}testfiles\\{filename}";
+        }
+
+        private static string GetBoundary(MediaTypeHeaderValue contentType, int lengthLimit)
+        {
+            var boundary = HeaderUtilities.RemoveQuotes(contentType.Boundary);
+            if (string.IsNullOrWhiteSpace(boundary.Value))
+            {
+                throw new InvalidDataException("Missing content-type boundary.");
+            }
+
+            if (boundary.Length > lengthLimit)
+            {
+                throw new InvalidDataException(
+                    $"Multipart boundary length limit {lengthLimit} exceeded.");
+            }
+
+            return boundary.Value;
+        }
+
+    }
+}
